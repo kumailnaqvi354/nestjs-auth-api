8954f0f846e525d606da552c0482416ae0b1e4e8
kumailnaqvi354 <komailnaqvi616@gmail.com>
2023-09-08 12:10:18 +0500

    Added User resources and User Schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
