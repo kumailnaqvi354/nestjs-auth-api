a64db2bee313821bf59b053ae800b7e7ff856e7b
kumailnaqvi354 <komailnaqvi616@gmail.com>
2023-09-08 13:41:29 +0500

    user services updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), UsersModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,115 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<any> {
+    // check if user already exists
+    const userExists = await this.usersService.findByUsername(
+      createUserDto.username,
+    );
+
+    if (userExists) {
+      throw new BadRequestException('User Already exists');
+    }
+
+    // hash password
+    const hash = await this.hashData(createUserDto.password);
+    const newUser = await this.usersService.create({
+      ...createUserDto,
+      password: hash,
+    });
+    const tokens = await this.getTokens(newUser._id, newUser.username);
+
+    return tokens;
+  }
+
+  async signIn(data: AuthDto) {
+    // check if user exist
+    const user = await this.usersService.findByUsername(data.username);
+
+    if (!user) {
+      throw new BadRequestException('User does not exists');
+    }
+
+    const passwordMatches = await argon2.verify(user.password, data.password);
+    if (!passwordMatches) {
+      throw new BadRequestException('Password is Incorrect');
+    }
+    const tokens = await this.getTokens(user._id, user.username);
+    return tokens;
+  }
+
+  async logout(userId: string) {
+    return this.usersService.update(userId, { refreshToken: null });
+  }
+
+  hashData(data: string) {
+    return argon2.hash(data);
+  }
+
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    const hashRefreshToken = await this.hashData(refreshToken);
+    await this.usersService.update(userId, { refreshToken: hashRefreshToken });
+  }
+
+  async getTokens(userId: string, username: string) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+          expiresIn: '15m',
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+          expiresIn: '7d',
+        },
+      ),
+    ]);
+
+
+    return {accessToken, refreshToken}
+  }
+
+  create(createAuthDto: AuthDto) {
+    return 'This action adds a new auth';
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
