a320995c5d375a65577b0e2bdf8390d411c66c86
kumailnaqvi354 <komailnaqvi616@gmail.com>
2023-09-08 13:44:53 +0500

    auth controller updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -93,23 +93,4 @@ export class AuthService {
     return {accessToken, refreshToken}
   }
 
-  create(createAuthDto: AuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
