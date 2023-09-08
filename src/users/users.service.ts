9a300774c11d8220552fed79c5c3768f9f8e61f6
kumailnaqvi354 <komailnaqvi616@gmail.com>
2023-09-08 12:17:25 +0500

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@Injectable(User.name) private userModel: Model<UserDocument>){}
+
+}   
